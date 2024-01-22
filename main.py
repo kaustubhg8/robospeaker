9e5ced9c90664f55fd9b4eb8ce87ccbd44dd134e
Kaustubh <garud.kaustubh@gmail.com>
2024-01-22 20:54:39 +0530

    project done

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Welcome to the Robo Speaker")
+    while True:
+        x = input("Enter what you want me to speak: ")
+        if x == 'q':
+            os.system('wsay "bye bye"')
+            break
+        command = f'wsay "{x}"'
+        os.system(command)
